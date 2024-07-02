8bc1a165da114c3eb206e24316b5740a7e01b214
chenliangjin <844207857@qq.com>
2024-07-03 01:32:24 +0800

    update codes

--- corners_det/src/parsing_bin.cpp
+++ corners_det/src/parsing_bin.cpp
@@ -6,24 +6,18 @@ using namespace std;
 using namespace cv;
 
 
-int main() {
-    int width = 960;
-    int height = 512;
-    int image_size = width * height;
-
-    float* buffer = new (nothrow) float[image_size];
+void readBin(const char* bin_path, float* buffer, size_t width, size_t height) {
+    size_t image_size = width * height;
     if (buffer == nullptr) {
         cerr << "内存分配失败" << endl;
-        return -1;
     } else {
         cout << "内存分配成功" << endl;
     }
 
-    fstream bin_file("heatmap.bin", ios::in | ios::binary);
+    fstream bin_file(bin_path, ios::in | ios::binary);
     if (!bin_file) {
         cerr << "无法打开文件" << endl;
         delete[] buffer;
-        return -1;
     }
 
     // check file size
@@ -35,7 +29,6 @@ int main() {
         cerr << "文件大小不匹配，预期大小: " << image_size*sizeof(float) << "字节，实际大小: " << file_size << "字节" << endl;
         bin_file.close();
         delete[] buffer;
-        return -1; 
     }
 
     bin_file.read(reinterpret_cast<char*>(buffer), image_size*sizeof(float));
@@ -43,13 +36,8 @@ int main() {
         cerr << "读取文件失败" << endl;
         bin_file.close();
         delete[] buffer;
-        return -1;
     }
     bin_file.close();
-
-    for (int i=0; i<10; i++) {
-        cout << buffer[i] << " ";
-    }
 
     Mat image(height, width, CV_32FC1, buffer);
     Mat image_8bit;
@@ -57,6 +45,17 @@ int main() {
 
     imshow("image", image_8bit);
     waitKey(0);
+}
+
+
+int main() {
+    int width = 960;
+    int height = 512;
+    int image_size = width * height;
+    const char* bin_path = "heatmap.bin";
+
+    float* buffer = new (nothrow) float[image_size];
+    readBin(bin_path, buffer, width, height);
 
     delete[] buffer;
     return 0;
